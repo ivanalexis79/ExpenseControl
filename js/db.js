774f86fa3d8755a1227cd605b092e85d482b7849
ivanalexis79 <ivanalexis.gonzalez@gmail.com>
2025-05-26 23:22:30 -0400

    Update db.js

--- js/db.js
+++ js/db.js
@@ -2,10 +2,12 @@ const dbRequest = indexedDB.open("GastosDB", 1);
 
 dbRequest.onupgradeneeded = (event) => {
     let db = event.target.result;
-    db.createObjectStore("gastos", { keyPath: "id", autoIncrement: true });
+    // Verificar si el objeto almacenado ya existe antes de crearlo
+    if (!db.objectStoreNames.contains("gastos")) {
+        db.createObjectStore("gastos", { keyPath: "id", autoIncrement: true });
+    }
 };
 
 dbRequest.onsuccess = (event) => {
-    let db = event.target.result;
     console.log("Base de datos lista.");
 };
