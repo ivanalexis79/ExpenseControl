655e497cd039c6ffae2219c103da07b8586f13e8
ivanalexis79 <ivanalexis.gonzalez@gmail.com>
2025-05-26 23:29:49 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/service-worker.js")
+    .then(() => console.log("Service Worker registrado correctamente."))
+    .catch((error) => console.error("Error al registrar Service Worker:", error));
+}
+
+
+
 let db;
 
 const dbRequest = indexedDB.open("GastosDB", 1);
