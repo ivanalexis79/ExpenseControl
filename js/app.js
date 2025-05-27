df8f844fe1c5ad228b814df45a5979c33b58cc4c
ivanalexis79 <ivanalexis.gonzalez@gmail.com>
2025-05-26 23:34:23 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/service-worker.js")
+    navigator.serviceWorker.register("service-worker.js")
     .then(() => console.log("Service Worker registrado correctamente."))
     .catch((error) => console.error("Error al registrar Service Worker:", error));
 }
